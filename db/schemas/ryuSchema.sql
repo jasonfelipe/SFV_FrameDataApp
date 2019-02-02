141cd33e8f91fd8709a3d888fc7ea6d0dc82e2a1
Legend <jasonrfelipe@gmail.com>
2019-02-02 13:47:22 -0700

    Added mysql schema and seeds

---
+++ db/schemas/ryuSchema.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS sfv_framedatadb;
+
+CREATE DATABASE sfv_framedatadb;
+
+USE sfv_framedatadb;
+
+create table ryu (
+	id int auto_increment not null,
+	move varchar (255) not null,
+    startup varchar (100), 
+    active varchar (100),
+    recovery varchar (100),
+    onHit varchar (100),
+    onBlock varchar(100),
+    damage varchar (100),
+    stun varchar(100),
+    attackType varchar (100),
+    primary key (id)
+    );
+    
---
+++ db/seeds/ryuSeeds.sql
@@ -0,0 +1,88 @@
+-- Stand Attacks
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Stand Light Punch", 3, 2, 7, 4, 2, 30, 70, "H");
+
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Stand Medium Punch", 5, 3, 10, 7, 1, 60, 100, "H");
+
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Stand Heavy Punch", 8, 3, 20, 3, -1, 90, 150, "H");
+
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Stand Light Kick", 4, 3, 11, -1, -2, 30, 70, "H");
+
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Stand Medium Kick", 8, 3, 16, 2, -2, 60, 100, "H");
+
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Stand Heavy Kick", 10, 2, 20, 4, -1, 90, 150, "H");
+
+ -- Crouch Attacks
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Crouch Light Punch", 4, 3, 5, 3, 2, 30, 70, "H");
+
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Crouch Medium Punch", 6, 3, 12, 4, 2, 60, 100, "H");
+
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Crouch Heavy Punch", 6, 4, 24, -7, -10, 90, 150, "H");
+
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Crouch Light Kick", 4, 2, 7, 2, 1, 20, 70, "L");
+
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Crouch Medium Kick", 7, 3, 13, 1, -3, 50, 100, "L");
+
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Crouch Heavy Kick", 8, 2, 22, "KD", -11, 90, 150, "L");
+
+-- Jump Attacks
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Jump Light Punch", 3, 6, null, null, null, 40, 70, "M");
+
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Jump Medium Punch", 7, 8, null, null, null, "30 + 30", "50 + 50", "M");
+
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Jump Heavy Punch", 7, 7, null, null, null, 90, 150, "M");
+
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Jump Light Kick", 4, 6, null, null, null, 40, 70, "M");
+
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Jump Medium Kick", 6, 4, null, null, null, 60, 100, "M");
+
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Jump Heavy Kick", 9, 5, null, null, null, 90, 150, "M");
+
+-- Unique Moves
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Collarbone Breaker (F + MP)", 22, "1 & 3", 17, 1, -6, "30 + 30", "60 + 40", "M + M");
+
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Solar Plexus Strike (F + HP)", 17, "2 (1) & 2", 18, 8, -2, "40 + 40", "75 + 75", "H + H");
+
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Axe Kick (B + HK)", 9, "3 (3) & 4", 16, 4, -2, "40 + 40", "60 + 90", "H + H");
+
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+value ("Jodan Sanrengeki 1st (s.MP -> s.HP)", "5 + 9", 2, 28, -9, -16, "60 + 63", "100 + 90","H + H");
+
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Jodan Sanrengeki 2nd (s.MP -> s.HP -> s.HK)", "5 + 9 + 9", 2, 26, "KD", -7, "60 + 63 + 56", "100 + 90 + 80", "H + H + H (Crouchable)");
+
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Jodan Nirengeki (s.HP -> s.HK)", "8 + 9", 2, 26, "KD", -7, "90 + 63", "150 + 90", "H + H (Crouchable)");
+
+-- Throw
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Shoulder Throw (F + Throw)", 5, 3, 17, "KD", "KD", 130, 150, "T");
+
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Somersault Throw (B + Throw)", 5, 3, 17, "KD", "KD", 150, 200, "T");
+
+-- V Skill
+insert into ryu (move, startup, active, recovery, onHit, onBlock, damage, stun, attackType)
+values ("Mind's Eye (V Skill)", "3 (1)", 7, 32, null, null, null, null, null);
+
+
