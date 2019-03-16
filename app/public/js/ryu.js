db327398534e4d89e4f9246fb7e41d2ceea32302
Legend <jasonrfelipe@gmail.com>
2019-03-16 14:26:20 -0700

    more testing

--- app/public/js/ryu.js
+++ app/public/js/ryu.js
@@ -1,4 +1,7 @@
-let saveddata;
+let allCombos = [];
+let comboArray = [];
+let totalDamage = ""
+
 
 $.get("/api/ryu", function (data) {
     // console.log(data);
@@ -7,6 +10,16 @@ $.get("/api/ryu", function (data) {
         tableCreator.createRows(element)
     );
 });
+
+
+$(document).on('click', ".move-button", function () {
+    let moveName = $(this).attr('id').trim();
+    comboArray.push(moveName);
+    console.log(moveName);
+    contCombo(moveName);
+});
+
+
 
 const tableCreator = {
     createRows: function (item) {
@@ -37,7 +50,6 @@ const tableCreator = {
         `Attack Type: ${item.attackType}`]
 
 
-
         this.advantageCheck(item.onHit);
         $(move).css('cursor', 'pointer').attr('data-toggle', 'modal');
         $(move).on('click', function () {
@@ -51,6 +63,7 @@ const tableCreator = {
                 frameDataList.append(`<li>${frameDataCol[f]}</li>`);
             }
             $('#frame-data-col').append(frameDataList);
+            
             if (item.notes) {
                 notesList = $('<ul>');
                 let moveNotes = item.notes.split('. ')
@@ -95,6 +108,9 @@ $('#combo-page-link').on('click', function () {
 $('#create-combo-button').on('click', function () {
     $('#cbutton-section').empty()
     $('#combo-modal').modal('show');
+    comboArray = [];
+    console.log(comboArray);
+    $('#combo-view').empty();
     makeCombo()
 });
 
@@ -107,54 +123,57 @@ function makeButtons(str) {
     $('#cbutton-section').append(button);
 }
 
+
+
 function makeCombo() {
     $.get('/api/ryu/create', function (data) {
         data.forEach(element => {
             // console.log(element.move);
             makeButtons(element.move)
         });
-
-        $(document).on('click', ".move-button", function () {
-            let moveName = $(this).attr('id').trim();
-            contCombo(moveName);
-            $('#combo-view').empty();
-        });
     });
 }
 
-function contCombo(moveName){
-    let comboArray = [];
+//Stuff to do ~
+// Make the combo go in as an array. Each Click of the button, the move name should go into an array, and then 
+// refresh the buttons in the modal. 
+// Once the array hits a length of 3 (for now), the combo should end. 
+
+function contCombo(moveName) {
     $.get('/api/ryu/combo', function (list) {
         list.forEach(element => {
             if (moveName === element.move) {
                 // console.log("This matched", element)
                 let moveHit = parseInt(element.onHit);
                 let moveDamage = parseInt(element.damage);
-
+                let comboList;
 
 
                 $('#cbutton-section').empty();
-    
-                
-                
-                if (comboArray.length > 3) {
+
+
+
+                if (comboArray.length < 3) {
                     list.forEach(nextMove => {
-                        if(moveHit > 3){
+                        if (moveHit > 3) {
                             let nextMoveStartUp = parseInt(nextMove.startup);
                             if (moveHit >= nextMoveStartUp) {
                                 // console.log("Leftovers...", nextMove);
-                                makeButtons(nextMove.move);  
+                                makeButtons(nextMove.move);
                             }
-    
-                        }else{
+
+                        } else {
                             return console.log('Combo Finished');
                         }
-                            
+
                     });
                 }
 
-            $('#combo-view').text(comboArray);                
-                
+                comboList = $('<ul>');
+                for (let m = 0; m < comboArray.length; m++) {
+                    comboList.append(`<li>${comboArray[m]}</li>`);
+                }
+                $('#combo-view').append(comboList);
             }
         });
     });
